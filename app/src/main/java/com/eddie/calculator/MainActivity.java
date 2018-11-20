afd4ae898b667d0a2de3de86eedd5a31d9570469
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-11-20 20:05:27 +0200

    Initial commit

---
+++ app/src/main/java/com/eddie/calculator/MainActivity.java
@@ -0,0 +1,62 @@
+package com.eddie.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickDiv(View view) {
+    }
+
+    public void clickSeven(View view) {
+    }
+
+    public void clickFour(View view) {
+    }
+
+    public void clickNine(View view) {
+    }
+
+    public void clickZero(View view) {
+    }
+
+    public void clickResult(View view) {
+    }
+
+    public void clickClear(View view) {
+    }
+
+    public void clickThree(View view) {
+    }
+
+    public void clickTwo(View view) {
+    }
+
+    public void clickOne(View view) {
+    }
+
+    public void clickSix(View view) {
+    }
+
+    public void clickFive(View view) {
+    }
+
+    public void clickEight(View view) {
+    }
+
+    public void clickMinus(View view) {
+    }
+
+    public void clickPlus(View view) {
+    }
+
+    public void clickStar(View view) {
+    }
+}
